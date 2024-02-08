cd058d427c0ee09b0a5bf4cbf9d43483d7b89cc8
Eurekahah <A984036428@gmail.com>
2024-02-08 10:34:09 +0800

    增加计算客流量，删除无用代码段，增添注释说明

--- GetIn_Off.py
+++ GetIn_Off.py
@@ -1,14 +1,23 @@
 import pandas as pd
+################################
+# 需要下载包pandas配置到解释器环境之中
+# 源路径需要csv数据
+# 编码方式采用GB2312，便于使用Excel打开
+################################
+
+
+source_path = 'source_data.csv'
+destination_path = 'version7_merged_data.csv'
+encoding_methods = 'GB2312'
 
 # 1. 读取源表数据
-source_data = pd.read_csv('source_data.csv', encoding='GB2312')
+source_data = pd.read_csv(source_path, encoding=encoding_methods)
 
 # 2. 数据清洗和预处理
 # 假设不需要清洗和预处理，源表数据已经干净
 
 # 3. 创建生成表框架
-columns = ['班次', '站点序号', '上车人数', '下车人数', '客流量']
-result_table = pd.DataFrame(columns=columns)
+# 直接利用源表进行生成
 
 # 4. 数据分组和统计
 # 统计上车人数
@@ -20,22 +29,20 @@ down_grouped_data = down_grouped_data.rename(columns={'下车站点序号': '站
 
 # 合并两个表
 merged_data = pd.merge(grouped_data, down_grouped_data,how='outer', on=['班次', '站点序号'], suffixes=('_上车', '_下车')).fillna(value=0)
+# 浮点数化为整型
 merged_data['上车人数'] = merged_data['上车人数'].fillna(0).astype(int)
+
 merged_data['下车人数'] = merged_data['下车人数'].fillna(0).astype(int)
 
 merged_data_sorted = merged_data.sort_values(by=['班次', '站点序号'])
 
+# 计算客流量
+merged_data_sorted['客流量']=merged_data_sorted['上车人数']-merged_data_sorted['下车人数']
 # 对缺失值进行填充
 merged_data.fillna(0, inplace=True)
 
-# # 计算客流量
-# merged_data['客流量'] = merged_data['上车人数'] - merged_data['下车人数']
-
-
-
 # 输出生成表
-
 print(merged_data_sorted)
 
 # 将结果保存为CSV文件
-merged_data_sorted.to_csv('version7_merged_data.csv', index=False, encoding='GB2312')
+merged_data_sorted.to_csv(destination_path, index=False, encoding=encoding_methods)
