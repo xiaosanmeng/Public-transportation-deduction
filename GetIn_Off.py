9709639d221d32deebd446ceb10fd64741e5944f
Eurekahah <A984036428@gmail.com>
2024-02-08 10:21:04 +0800

    计算每班次各个站点的上下车人数

---
+++ GetIn_Off.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+# 1. 读取源表数据
+source_data = pd.read_csv('source_data.csv', encoding='GB2312')
+
+# 2. 数据清洗和预处理
+# 假设不需要清洗和预处理，源表数据已经干净
+
+# 3. 创建生成表框架
+columns = ['班次', '站点序号', '上车人数', '下车人数', '客流量']
+result_table = pd.DataFrame(columns=columns)
+
+# 4. 数据分组和统计
+# 统计上车人数
+grouped_data = source_data.groupby(['班次', '上车站点序号']).size().reset_index(name='上车人数')
+grouped_data = grouped_data.rename(columns={'上车站点序号': '站点序号'})
+# 统计下车人数
+down_grouped_data = source_data.groupby(['班次', '下车站点序号']).size().reset_index(name='下车人数')
+down_grouped_data = down_grouped_data.rename(columns={'下车站点序号': '站点序号'})
+
+# 合并两个表
+merged_data = pd.merge(grouped_data, down_grouped_data,how='outer', on=['班次', '站点序号'], suffixes=('_上车', '_下车')).fillna(value=0)
+merged_data['上车人数'] = merged_data['上车人数'].fillna(0).astype(int)
+merged_data['下车人数'] = merged_data['下车人数'].fillna(0).astype(int)
+
+merged_data_sorted = merged_data.sort_values(by=['班次', '站点序号'])
+
+# 对缺失值进行填充
+merged_data.fillna(0, inplace=True)
+
+# # 计算客流量
+# merged_data['客流量'] = merged_data['上车人数'] - merged_data['下车人数']
+
+
+
+# 输出生成表
+
+print(merged_data_sorted)
+
+# 将结果保存为CSV文件
+merged_data_sorted.to_csv('version7_merged_data.csv', index=False, encoding='GB2312')
